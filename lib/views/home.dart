c38c6f345c837cabce47e4394a5bc6cb7099b863
Prosenjit Swarnakar <prosenjitswarnakar2002@gmail.com>
2022-10-16 13:31:37 +0530

    Created JSON format & key value pairs

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:wallpaper_store/data/data.dart';
 import 'package:wallpaper_store/widgets/widget.dart';
-
 import '../model/categories_model.dart';
-
+import 'package:http/http.dart' as http;
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -12,15 +14,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   List<CategoriesModel> categories = [];
+
+
+  getTrendingWallpapers() async {
+    var response = await http.get(Uri.parse("https://api.pexels.com/v1/curated?per_page=15&page=1"
+    ),headers: {
+      "Authorization" : apiKey
+    });
+    //if (kDebugMode)
+    //{
+      //print(response.body.toString());
+    //}
+    Map<String, dynamic> jsondata = jsonDecode(response.body);
+  }
 
   @override
   void initState() {
+    getTrendingWallpapers();
     categories = getCategories();
     // TODO: implement initState
     super.initState();
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -36,71 +52,81 @@ class _HomeState extends State<Home> {
           children: [
             Container(
               decoration: BoxDecoration(
-              color: const Color(0xfff5f8fd),
-                borderRadius: BorderRadius.circular(30)
-              ),
+                  color: const Color(0xfff5f8fd),
+                  borderRadius: BorderRadius.circular(30)),
               padding: const EdgeInsets.symmetric(horizontal: 22),
               margin: const EdgeInsets.symmetric(horizontal: 24),
-              child: Row(children: const [
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Search Wallpapers",
-                      border: InputBorder.none
+              child: Row(
+                children: const [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Search Wallpapers",
+                          border: InputBorder.none),
                     ),
                   ),
-                ),
-                Icon(Icons.search)
-              ],),
+                  Icon(Icons.search)
+                ],
+              ),
             ),
-
-            const SizedBox(height: 16,),
+            const SizedBox(
+              height: 16,
+            ),
             Container(
               height: 80,
               child: ListView.builder(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                itemCount: categories.length,
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  itemCount: categories.length,
                   shrinkWrap: true,
                   scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index){
-                  return CategoriesTile(
-                      title: categories[index].categoriesName,
-                      imageUrl: categories[index].imageUrl);
+                  itemBuilder: (context, index) {
+                    return CategoriesTile(
+                        title: categories[index].categoriesName,
+                        imageUrl: categories[index].imageUrl);
                   }),
             )
-
-
-
-          ],),
+          ],
+        ),
       ),
     );
   }
 }
 
 class CategoriesTile extends StatelessWidget {
-
   final String imageUrl, title;
 
-  const CategoriesTile({Key? key,  required this.title,required this.imageUrl}) : super(key: key);
-
-
+  const CategoriesTile({Key? key, required this.title, required this.imageUrl})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(right: 6),
-      child: Stack(children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-            child: Image.network(imageUrl, height: 60, width: 120,
-            fit: BoxFit.cover,)),
-        Container(
-          color: Colors.black26,
-          height: 60, width: 120,
-          alignment: Alignment.center,
-          child: Text(title, style: const TextStyle(color: Colors.white, fontSize: 18,fontWeight: FontWeight.w600),),)
-      ],),
+      margin: const EdgeInsets.only(right: 6),
+      child: Stack(
+        children: [
+          ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.network(
+                imageUrl,
+                height: 60,
+                width: 120,
+                fit: BoxFit.cover,
+              )),
+          Container(
+            color: Colors.black26,
+            height: 60,
+            width: 120,
+            alignment: Alignment.center,
+            child: Text(
+              title,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
-
